7941ac51f6f4266512366bf90da9b996d32e2e57
Bob Violier <bob.violier@mobiletulip.com>
2013-08-23 14:22:06 +0200

    Move messagebird class (typo) and convert to PSR standard

--- example.php
+++ example.php
@@ -1,49 +1,49 @@
 <?php
-	/**
-	 * =======================================================================
-	 *  File:       example_form.php
-	 *  Created:     2010-07-06
-	 *  Author:      MessageBird B.V.
-	 *  Version:     v1.2 - 04-03-2013
-	 *
-	 *  More information? Go to www.messagebird.com/sms-api
-	 *
-	 * This class requires that you have PHP 5.1.x or higher installed.
-	 * ========================================================================
-	*/
+/**
+ * =======================================================================
+ *  File:       example_form.php
+ *  Created:     2010-07-06
+ *  Author:      MessageBird B.V.
+ *  Version:     v1.2 - 04-03-2013
+ *
+ *  More information? Go to www.messagebird.com/sms-api
+ *
+ * This class requires that you have PHP 5.1.x or higher installed.
+ * ========================================================================
+ */
 
-	require_once 'lib/class.MessageBird.php';
+require_once 'lib/class.MessageBird.php';
 
-	// Set the Messabeird username and password, and create an instance of the MessageBird class
-	$sms = new MessageBird( 'username', 'password' );
+// Set the Messabeird username and password, and create an instance of the MessageBird class
+$sms = new MessageBird('username', 'password');
 
-	// Set the sender, could be an number (16 numbers) or letters (11 characters)
-	$sms->setSender ( 'YourSender' );
+// Set the sender, could be an number (16 numbers) or letters (11 characters)
+$sms->setSender('YourSender');
 
-	// Add the destination mobile number.
-	// This method can be called several times to add have more then one recipient for the same message
-	$sms->addDestination ( '31600000000' );
+// Add the destination mobile number.
+// This method can be called several times to add have more then one recipient for the same message
+$sms->addDestination('31600000000');
 
-	// Set an reference, optional
-	$sms->setReference ( '123456789' );
+// Set an reference, optional
+$sms->setReference('123456789');
 
-	// Set a schedule date-time, optional
-	// $sms->setTimestamp('2014-01-01 10:02');
+// Set a schedule date-time, optional
+// $sms->setTimestamp('2014-01-01 10:02');
 
-	// Replace non GSM-7 characters by appropriate valid GSM-7 characters
-	// $sms->setReplacechars(false);
+// Replace non GSM-7 characters by appropriate valid GSM-7 characters
+// $sms->setReplacechars(false);
 
-	// If you want a dlr notification of the message send to another url then that you have set on the web site, you can use this parameter. Don't forget to set a reference!
-	// $sms->setDlrUrl('http://www.example.com/dlr_url.php');
+// If you want a dlr notification of the message send to another url then that you have set on the web site, you can use this parameter. Don't forget to set a reference!
+// $sms->setDlrUrl('http://www.example.com/dlr_url.php');
 
-	// If $test is TRUE, then the message is not actually sent or scheduled, and there will be no credits deducted.
-	// $sms->setTest(true);
+// If $test is TRUE, then the message is not actually sent or scheduled, and there will be no credits deducted.
+// $sms->setTest(true);
 
-	// Send the message to the destination(s)
-	$sms->sendSms ( 'This is a test message' );
+// Send the message to the destination(s)
+$sms->sendSms('This is a test message');
 
-	echo "\nResponse:";
-	echo "\n" . $sms->getResponseCode ();
-	echo "\n" . $sms->getResponseMessage ();
-	echo "\n" . $sms->getCreditBalance ();
-	echo "\n";
+echo "\nResponse:";
+echo "\n" . $sms->getResponseCode();
+echo "\n" . $sms->getResponseMessage();
+echo "\n" . $sms->getCreditBalance();
+echo "\n";
--- example_form.php
+++ example_form.php
@@ -1,116 +1,95 @@
 <?php
-	/**
-	 * =======================================================================
-	 *  File:       example_form.php
-	 *  Created:     2010-07-06
-	 *  Author:      MessageBird B.V.
-	 *  Version:     v1.2 - 04-03-2013
-	 *
-	 *  More information? Go to www.messagebird.com/sms-api
-	 *
-	 *  This class requires that you have PHP 5.1.x or higher installed.
-	 * ========================================================================
-	*/
+/**
+ * =======================================================================
+ *  File:       example_form.php
+ *  Created:     2010-07-06
+ *  Author:      MessageBird B.V.
+ *  Version:     v1.2 - 04-03-2013
+ *
+ *  More information? Go to www.messagebird.com/sms-api
+ *
+ *  This class requires that you have PHP 5.1.x or higher installed.
+ * ========================================================================
+ */
 
-	require_once 'lib/class.MessageBird.php';
+require_once 'lib/class.MessageBird.php';
 
 // Set the MessageBird username and password. Will be used later in the script
-	$username = 'username';
-	$password = 'password';
+$username = 'username';
+$password = 'password';
 
 // Prevent default username/password example settings to call the API
-	if ( $username == 'username' || $password == 'password' )
-	{
-		echo '<br />You need to enter the correct username and password in example_form.php before you can use this example!';
-		exit;
-	}
+if ($username == 'username' || $password == 'password') {
+    echo '<br />You need to enter the correct username and password in example_form.php before you can use this example!';
+    exit;
+}
 
 // Only send message when this script is accessed using an HTTP POST
-	if ( ! empty( $_POST ) )
-	{
-		// Check if destination is posted
-		if ( ! empty( $_POST['destination'] ) )
-		{
-			$destination = $_POST['destination'];
-		}
-		else
-		{
-			$destination = NULL;
-		}
+if (! empty($_POST)) {
+    // Check if destination is posted
+    if (! empty($_POST['destination'])) {
+        $destination = $_POST['destination'];
+    } else {
+        $destination = null;
+    }
 
-		// Check if message is posted
-		if ( ! empty( $_POST['message'] ) )
-		{
-			$message = $_POST['message'];
-		}
-		else
-		{
-			$message = NULL;
-		}
+    // Check if message is posted
+    if (! empty($_POST['message'])) {
+        $message = $_POST['message'];
+    } else {
+        $message = null;
+    }
 
-		// Check if sender is posted
-		if ( ! empty( $_POST['sender'] ) )
-		{
-			$sender = $_POST['sender'];
-		}
-		else
-		{
-			$sender = NULL;
-		}
+    // Check if sender is posted
+    if (! empty($_POST['sender'])) {
+        $sender = $_POST['sender'];
+    } else {
+        $sender = null;
+    }
 
-		// Check if reference is posted
-		if ( ! empty( $_POST['reference'] ) )
-		{
-			$reference = $_POST['reference'];
-		}
-		else
-		{
-			$reference = NULL;
-		}
+    // Check if reference is posted
+    if (! empty($_POST['reference'])) {
+        $reference = $_POST['reference'];
+    } else {
+        $reference = null;
+    }
 
-		// If we have the required parameters, we can send a message.
-		if ( $destination !== NULL && $message !== NULL && $sender !== NULL )
-		{
-			$sms = new MessageBird( $username, $password );
+    // If we have the required parameters, we can send a message.
+    if ($destination !== null && $message !== null && $sender !== null) {
+        $sms = new MessageBird($username, $password);
 
-			// Add the destination mobile number.
-			// This method can be called several times to add have more then one recipient for the same message
-			$sms->addDestination ( $destination );
+        // Add the destination mobile number.
+        // This method can be called several times to add have more then one recipient for the same message
+        $sms->addDestination($destination);
 
-			if ( $sender !== NULL )
-			{
-				// Set the sender, could be an number (16 numbers) or letters (11 characters)
-				$sms->setSender ( $sender );
-			}
+        if ($sender !== null) {
+            // Set the sender, could be an number (16 numbers) or letters (11 characters)
+            $sms->setSender($sender);
+        }
 
-			if ( $reference !== NULL )
-			{
-				// Set an reference
-				$sms->setReference ( $reference );
-			}
+        if ($reference !== null) {
+            // Set an reference
+            $sms->setReference($reference);
+        }
 
-			// Send the message to the destination(s)
-			$sms->sendSms ( $message );
+        // Send the message to the destination(s)
+        $sms->sendSms($message);
 
-			// Output the response to the browser
-			echo '<br />Response Info:';
-			echo '<br />Response Code: ' . $sms->getResponseCode ();
-			echo '<br />Response Message: ' . $sms->getResponseMessage ();
-			echo '<br />Credit Balance: ' . $sms->getCreditBalance ();
-			echo '<br />';
+        // Output the response to the browser
+        echo '<br />Response Info:';
+        echo '<br />Response Code: ' . $sms->getResponseCode();
+        echo '<br />Response Message: ' . $sms->getResponseMessage();
+        echo '<br />Credit Balance: ' . $sms->getCreditBalance();
+        echo '<br />';
 
-		// There is no destination or message posted, we realy need those two to work.
-		}
-		else
-		{
-			echo '<br />No destination, sender or message given!';
-			echo '<br />You must enter an destination, sender and message!';
-		}
+        // There is no destination or message posted, we realy need those two to work.
+    } else {
+        echo '<br />No destination, sender or message given!';
+        echo '<br />You must enter an destination, sender and message!';
+    }
 
 // It seems there is no POST, and this example script only works with an HTTP POST.
-	}
-	else
-	{
-		echo '<br />This script needs to be called via an HTTP POST!';
-		echo '<br />Use <a href="example_form.html">example_form.php</a> to test this example script.';
-	}
+} else {
+    echo '<br />This script needs to be called via an HTTP POST!';
+    echo '<br />Use <a href="example_form.html">example_form.php</a> to test this example script.';
+}
